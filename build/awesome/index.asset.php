585f030e9b4623d69fa53d95a3f0cf60260c08c7
parich suriya <parich.su@rmu.ac.th>
2026-03-28 10:34:21 +0700

    add: one

---
+++ build/awesome/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '69ad5a6e697e2c108ba0');
---
+++ build/awesome/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e8a0eb12c45634839107');
